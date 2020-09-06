4f1294ab57d6f1d4f81de1525e1c6d1a043578ff
yunusakyuz <66246189+yunusakyuz@users.noreply.github.com>
2020-09-06 13:06:24 +0100

    Add files via upload

---
+++ karelerin_top.py
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i = 1, t = 0;
+
+    printf("Bir sayi girin: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+        t = t + (i * i);
+
+    printf("Sonuc: %d", t);
+
+    return 0;
+}
